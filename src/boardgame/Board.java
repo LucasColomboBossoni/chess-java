85b322c46b3d40b926bde48413cb890346041526
Lucas Colombo Bossoni <lucascolombobossoni@gmail.com>
2024-05-27 16:48:23 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.ChessPiece;
+
 public class Board {
 
 	private int rows;
@@ -43,6 +45,20 @@ public class Board {
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) { // verifica se a posição existe com base nos atributos linhas e colunas
